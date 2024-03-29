dcd49e73c4563a6749bbed9d802e12c194e839f3
Michal Macko <knowledgehole@gmail.com>
2024-03-29 19:49:48 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+$name = "Maria";
+ echo $name;
+?>  
+
+<!DOCTYPE html>
+<html>
+<body>
+<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Totam quasi, nam obcaecati ex eligendi tempore maiores ab, veniam magnam corporis repellendus ea deserunt cupiditate harum? Eligendi hic odio iure corrupti praesentium recusandae ipsam esse aspernatur reiciendis repellat, provident laborum earum quis dolor. Autem ipsa deleniti consequatur, quisquam nam nostrum! Officiis.</p>
+<h1>My first PHP page</h1>
+
+<h3>test test</h3>
+
+<?php
+echo "Hello World!";
+?>
+
+</body>
+</html>
