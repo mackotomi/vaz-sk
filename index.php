d8b832a83d36b700120b350afa5411640ceade98
Michal Macko <knowledgehole@gmail.com>
2024-03-31 18:52:42 +0200

    new changes from notebook

--- index.php
+++ index.php
@@ -1,19 +1,5 @@
-<?php
-$name = "Maria";
- echo $name;
-?>  
-
-<!DOCTYPE html>
-<html>
+<?php include 'head.php'; ?>
 <body>
-<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Totam quasi, nam obcaecati ex eligendi tempore maiores ab, veniam magnam corporis repellendus ea deserunt cupiditate harum? Eligendi hic odio iure corrupti praesentium recusandae ipsam esse aspernatur reiciendis repellat, provident laborum earum quis dolor. Autem ipsa deleniti consequatur, quisquam nam nostrum! Officiis.</p>
-<h1>My first PHP page</h1>
-
-<h3>this text has changed</h3>
-
-<?php
-echo "Hello World!";
-?>
-
+    <!-- Your HTML content goes here -->
 </body>
 </html>
