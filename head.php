33b2fb3e74754d014b9d4b856847ca3aa85b66f6
Michal Macko <knowledgehole@gmail.com>
2024-03-31 19:28:50 +0200

    new changes

--- head.php
+++ head.php
@@ -4,9 +4,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Vaz - Pneuservis</title>
-    <link rel="stylesheet" href="style.css">
     <!-- Google fonts 'Roboto - Thin, Light, Regular, Medium, Bold, Black' -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;700;900&display=swap" rel="stylesheet">
+    <!-- CSS styles made from SASS -->
+    <link rel="stylesheet" href="css/style.css">
 </head>
--- index.php
+++ index.php
@@ -1,5 +1,13 @@
 <?php include 'head.php'; ?>
 <body>
-    <!-- Your HTML content goes here -->
+    <div>
+        <h1>This is beginning of the vaz.sk</h1>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Excepturi dolorum quo nesciunt. Repudiandae nam praesentium labore enim blanditiis libero. Nisi sapiente animi consectetur excepturi dolore fuga doloribus? Eum nisi ipsa consequatur animi doloribus perferendis nesciunt! Soluta eos corporis numquam minus autem repudiandae eum rerum quae eveniet vitae? Error, nulla nisi?</p>
+
+        <h1>About vaz</h1>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Excepturi dolorum quo nesciunt. Repudiandae nam praesentium labore enim blanditiis libero. Nisi sapiente animi consectetur excepturi dolore fuga doloribus? Eum nisi ipsa consequatur animi doloribus perferendis nesciunt! Soluta eos corporis numquam minus autem repudiandae eum rerum quae eveniet vitae? Error, nulla nisi?
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Excepturi dolorum quo nesciunt. Repudiandae nam praesentium labore enim blanditiis libero. Nisi sapiente animi consectetur excepturi dolore fuga doloribus? Eum nisi ipsa consequatur animi doloribus perferendis nesciunt! Soluta eos corporis numquam minus autem repudiandae eum rerum quae eveniet vitae? Error, nulla nisi?
+        </p>
+    </div>
 </body>
 </html>
