fb8c9c9d827833869502625e8a898b634bedffc4
Michal Macko <knowledgehole@gmail.com>
2024-04-20 22:53:03 +0200

    added test page where font size variables were tested and all works fine

---
+++ test.php
@@ -0,0 +1,19 @@
+<!-- Head With Header--Component.php for Navigation -->
+<?php include 'includes/head--component.php'; ?>
+    
+<p class='one'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis delectus eligendi repellat hic illo rerum pariatur officia officiis, voluptatem cum consectetur commodi ad quia perspiciatis. Distinctio quaerat earum reiciendis beatae.</p>
+<br>
+<p class='two'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis delectus eligendi repellat hic illo rerum pariatur officia officiis, voluptatem cum consectetur commodi ad quia perspiciatis. Distinctio quaerat earum reiciendis beatae.</p>
+<br>
+<p class='three'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis delectus eligendi repellat hic illo rerum pariatur officia officiis, voluptatem cum consectetur commodi ad quia perspiciatis. Distinctio quaerat earum reiciendis beatae.</p>
+<br>
+<p class='four'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis delectus eligendi repellat hic illo rerum pariatur officia officiis, voluptatem cum consectetur commodi ad quia perspiciatis. Distinctio quaerat earum reiciendis beatae.</p>
+<br>
+<p class='five'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis delectus eligendi repellat hic illo rerum pariatur officia officiis, voluptatem cum consectetur commodi ad quia perspiciatis. Distinctio quaerat earum reiciendis beatae.</p>
+<br>
+<p class='six'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis delectus eligendi repellat hic illo rerum pariatur officia officiis, voluptatem cum consectetur commodi ad quia perspiciatis. Distinctio quaerat earum reiciendis beatae.</p>
+<br>
+<p class='seven'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis delectus eligendi repellat hic illo rerum pariatur officia officiis, voluptatem cum consectetur commodi ad quia perspiciatis. Distinctio quaerat earum reiciendis beatae.</p>
+<br>
+<!-- Footer Partial Including Body, Html End Tags-->
+<?php include 'includes/footer--component.php'; ?>
